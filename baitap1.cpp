6e5c7fce59d78219761ffa74d4a88270ef16188c
huycuong-code <huycuong20508@gmail.com>
2026-08-05 23:41:26 +0700

    Add baitap1: in thong tin ca nhan

---
+++ baitap1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "------------------------" << "\n";
+    cout << "Ho va ten: Nguyen Huy Cuong" << "\n";
+    cout << "Lop: C++ K01" << "\n";
+    cout << "Ngay sinh: 20/05/2008" << "\n";
+    cout << "So thich: hoc lap trinh, doc sach" << "\n";
+    cout << "Muc tieu: Thanh thao C va C++ de co nen tang tot hoc dai hoc" << "\n";
+    cout << "-------------------------" << "\n";
+}

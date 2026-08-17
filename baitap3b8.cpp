4bd04868d8719100c42412d06c8f13484ef4c914
huycuong-code <huycuong20508@gmail.com>
2026-08-17 12:50:45 +0700

    cap nhat

--- baitap3b8.cpp
+++ baitap3b8.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main(){
     int n;
     cout << "Nhap mot so nguyen duong n: ";
     cin >> n;
     if(n >= 2){
+        int dem = 0;
         for(int i = 2; i < n; i++){
             if(n % i == 0){
+                dem++;
                 cout << "Uoc so nho nhat lon hon 1 la: " << i;
                 break;
             }
-            else{
-                cout << "n la so nguyen to!";
-            }
+        }
+        if(dem != 1){
+            cout << "n la so nguyen to!";
         }
     }
     else{
